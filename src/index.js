183468712a7410016add5d0d0f1d25a44182a681
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-08-17 22:05:42 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Form from './components/Form'
 import ParentComponent from './components/ParentComponent'
+import DisplayData from './components/DisplayData'
 
 ReactDOM.render(
   <div>
